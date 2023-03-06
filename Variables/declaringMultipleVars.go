91afe5266cf5ec1f70a9f8d03af1703b70ead01c
Olek <94283587+olek-program@users.noreply.github.com>
2023-03-06 17:27:36 +0100

    Vars :D

---
+++ Variables/declaringMultipleVars.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// var name, name, name type = value, value, value
+
+var a, b, c int = 1, 2, 3
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
---
+++ Variables/declaringVars.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+var intVar int = 22
+
+func main() {
+	fmt.Println(intVar)
+}
