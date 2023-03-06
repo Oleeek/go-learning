2e151005457d9d217ff66cea2f1cc0c0c684a2af
Olek <94283587+olek-program@users.noreply.github.com>
2023-03-06 17:14:14 +0100

    comments

---
+++ Comments/comments.go
@@ -0,0 +1,20 @@
+// This is a comment
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world") //Prints "Hello, World"
+	fmt.Println("123") // Prints "123"
+	fmt.Println("123") // Prints "123"
+}
+
+/*
+func main() {
+	fmt.Println("Hello, world") //Prints "Hello, World"
+	fmt.Println("123") // Prints "123"
+	fmt.Println("123") // Prints "123"
+} */
+// multi line comments
